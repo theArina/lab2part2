5969464bf1ea0ce94637744fe888e3644076c260
Tom <ghost.with.soul@gmail.com>
2017-11-04 14:37:22 +0700

    added some func

--- lab2.2/lab2.2/Source.cpp
+++ lab2.2/lab2.2/Source.cpp
@@ -18,7 +18,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <malloc.h>
-#include <windows.h>
+//#include <windows.h>
 
 //struct Date
 //{
@@ -72,35 +72,51 @@ typedef struct Account
 	
 } Account;
 
-void fillAccounts(Account *accounts, int arraySize)
+void fillAcc(Account *accounts, int arraySize)
 {
 	for (int i = 0; i < arraySize; i++)
 		(accounts + i)->test = i;
 }
 
-void scanAccounts(Account *accounts, int id)
+void scanAcc(Account *accounts, int id)
 {
 	printf("please type a number:\n");
 	scanf("%d", &(accounts+id)->test);
 }
 
-void printAccounts(Account *accounts, int arraySize)
+void printAcc(Account *accounts, int id, int arraySize)
 {
-	for (int i = 0; i < arraySize; i++)
+	/*for (int i = 0; i < arraySize; i++)
+		printf("  %d", (accounts + i)->test);*/
+	if ((accounts + id)->test != 0)
+		printf("%d \n", (accounts + id)->test);
+}
+
+void clearingAccVars(Account *accounts, int id, int arraySize)
+{
+	if (id < arraySize)
+		(accounts + id)->test = 0;
+}
+
+void searchFreeVar(Account *accounts, int arraySize)
+{
+	for (int i = 0; i < arraySize, i++)
 	{
-		printf("  %d", &(accounts + i)->test);
+		if ((accounts + i)->test == 0)
+			//do actions
 	}
-	printf("\n");	
 }
 
 void main()
 {
-	int arraySize = 10;
-	//int id = 0;
+	int arraySize = 100;
+	int id = 0;
 	Account *accounts = (Account *)malloc(arraySize * sizeof(Account));
 
-	fillAccounts(accounts, arraySize);
-	printAccounts(accounts, arraySize);
+	printf("enter id: \n");
+	scanf("%d", &id);
+	fillAcc(accounts, arraySize);
+	printAcc(accounts, id, arraySize);
 
 	free(accounts);
 }
